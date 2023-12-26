e44302313e578148af595545e68645d61a8fb839
radyyyyy567 <127324238+radyyyyy567@users.noreply.github.com>
2023-12-26 20:30:44 +0700

    second

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -23,6 +23,27 @@ export const saveProduct = (req, res)=>{
     })
 }
 
+export const saveDataPDF = (req, res)=>{
+    
+    if(req.files === null) return res.status(400).json({msg: "No File Uploaded"});    
+    const file = req.files.file;
+    const fileSize = file.data.length;
+    const ext = path.extname(file.name);
+    const fileName = req.body.fileName; // Extract fileName from request body
+    const allowedType = ['.pdf'];
+
+    if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: "Invalid File"});
+    if(fileSize > 20000000) return res.status(422).json({msg: "Image must be less than 5 MB"});
+
+    file.mv(`./public/images/${fileName}`, async(err)=>{
+        if (err) {
+            return res.status(500).json({ msg: err.message });
+        } else {
+            return res.status(200).json({ msg: "File uploaded successfully"});
+        }
+    })
+}
+
 const currentDir = dirname(fileURLToPath(import.meta.url));
 
 export const getProduct = (req, res) => {
--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -2,13 +2,16 @@ import express from "express";
 import {
     downloadProduct,
     getProduct,
+    saveDataPDF,
     saveProduct,
+    
 } from "../controllers/ProductController.js";
 import { authenticateJWT } from '../middleware/auth.js'
 
 const router = express.Router();
 
 router.post('/products',authenticateJWT,  saveProduct);
+router.post('/products/pdf',authenticateJWT,  saveDataPDF);
 router.get('/products/:filename', getProduct);
 router.get('/products/download/:filename/:title', downloadProduct);
 
